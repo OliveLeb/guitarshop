8d7e7358fb5168f90f86456389cb7d9751771fbb
OliveLeb <lebel.olivier@wanadoo.fr>
2020-04-03 13:30:40 +0200

    quill emoji

--- src/app/service/commentaire.service.ts
+++ src/app/service/commentaire.service.ts
@@ -1,31 +1,31 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class CommentaireService {
   // url: string = "http://localhost:6400/commentaires";
 
   constructor(private http: HttpClient) {}
-  url = 'https://immense-tundra-17548.herokuapp.com/commentaires';
+  url = "https://immense-tundra-17548.herokuapp.com/commentaires";
   quillConfig: object = {
-    toolbar: [['bold', 'italic'], [{ color: [] }], ['emoji'], ['clean']]
+    toolbar: [["bold", "italic"], [{ color: [] }], ["clean"]]
   };
 
   getCommentaire() {
     return this.http.get(this.url);
   }
   getCommentaireById(_id: string) {
-    return this.http.get(this.url + '/' + _id);
+    return this.http.get(this.url + "/" + _id);
   }
   createCommentaire(post: object) {
     return this.http.post(this.url, post);
   }
   updateCommentaire(_id: string, data: object) {
-    return this.http.put(this.url + '/' + _id, data);
+    return this.http.put(this.url + "/" + _id, data);
   }
   deleteCommentaire(_id: string) {
-    return this.http.delete(this.url + '/' + _id);
+    return this.http.delete(this.url + "/" + _id);
   }
 }
